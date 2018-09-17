75e1a2599674f671e57591a9d47e0915e37dd7d4
Kleyton Ramos <kleytonmatosramos@gmail.com>
2018-09-17 19:38:58 -0300

    fix loop

--- Python/program.py
+++ Python/program.py
@@ -16,13 +16,10 @@ nameB = baseB['Sheet1']
 
 for line_A in nameA.rows:
     for cell_A in line_A:
-        # print("NomeA:" + cell_A.value)
         for line_B in nameB.rows:
             for cell_B in line_B:
-                print("Comparei A: {0} com B: {1}" .format(cell_A.value, cell_B.value))
-                # print("NomeB:" + cell_B.value)
-                # if levenshtein.compare(cell_A.value,cell_B.value) >= 75:
-                    # print("São iguais e passam no teste: {0}, {1}. Nível de semelhança: {2}% " .format(cell_A.value,cell_B.value,levenshtein.compare(cell_A.value,cell_B.value)))
+                if levenshtein.compare(cell_A.value,cell_B.value) >= 85:
+                    print("Levenshtein: {0}, {1}. Nível de semelhança: {2}% " .format(cell_A.value,cell_B.value,levenshtein.compare(cell_A.value,cell_B.value)))
 
         
 
