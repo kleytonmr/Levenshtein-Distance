edec6a4e5c6a0565793abddb7e6f93b769f48b1d
Kleyton Ramos <kleytonmatosramos@gmail.com>
2018-09-27 23:41:45 -0300

    fix generic class loop

--- Python/main.py
+++ Python/main.py
@@ -3,7 +3,7 @@ from program import Generic
 class Main:
 
     # Dataset
-    file = 'C:/Users/admin/Documents/Levenshtein-Distance/bases/base_teste.xlsx'
+    file    = 'C:/Users/admin/Documents/Levenshtein-Distance/bases/base_teste.xlsx'
     sheet_a = 'Banco digitação'
     sheet_b = 'Aux_Feira'
     
@@ -11,13 +11,13 @@ class Main:
     output = 'C:/Users/admin/Documents/Levenshtein-Distance/bases/output_V5.xlsx'
 
     # set name variable
-    country = 'Feira de Santana'
-    local = 'local'
-    nm_a = "nm_aluno"
-    serie_a = 'ano_curso'
+    country  = 'Feira de Santana'
+    local    = 'local'
+    nm_a     = "nm_aluno"
+    serie_a  = 'ano_curso'
     school_a = 'cod_inep'
-    nm_b = 'aluno'
-    serie_b = 'serie'
+    nm_b     = 'aluno'
+    serie_b  = 'serie'
     school_b = 'inep_escola'
 
     # Feira de Santana
